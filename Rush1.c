acb24cb4ca23fe910a87beac6c37d5220b13c13c
Nek97 <34401596+Nek97@users.noreply.github.com>
2020-11-01 13:36:42 +0100

    Update Rush1.c

--- Rush1.c
+++ Rush1.c
@@ -1,6 +1,8 @@
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+//*/*/*/*/*/*/*/*//
+#include <stdio.h>
+int size = 4;
 
 int ft_factorial(int n)
 {
@@ -112,29 +114,208 @@ void generate_borders(int **matrix, int range, int rows)
       if(matrix[i][j] > max)
         max = new_max(&reads, matrix, i, j);
     matrix[i][range + 1] = reads;
+    matrix[i][range + 2] = -1;
   }
 }
+/****************************************************************************/
 
-int main()
+void excludecombs(int **combs, int i)
 {
-  int max = 4;
+	int n;
+	int k;
+
+	n = 0;
+	while (n < size)
+	{	
+		k = 0;
+		while(k < ft_factorial(size))
+		{
+			if (combs[k][size+2] == -1)
+			{
+				if (combs[k][n] == combs[i][n])
+				{
+					combs[k][size + 2] = i; 
+				}
+			}
+			k++;
+		}
+		n++;
+	}
+}
+
+void clearcombs(int **combs, int i)
+{
+	int n;
+
+	n = 0;
+	while (n < ft_factorial(size))
+	{	
+		if (combs[n][size + 2] == i)
+		{
+			combs[n][size + 2] = -1;
+		}
+		n++;
+	}
+}
+
+int recursive(int **res, int **combs, int riga, int **inputs )
+{
+	int i =0;
+  int j;
+
+	if (riga == size)
+		return 1;
+	while (i < ft_factorial(size))
+	{
+      printf("Ao\n");
+		while (i < ft_factorial(size) && combs[i][size + 2] != -1)
+		{
+      printf("Skippo %d\n", i);
+      if(i == 23)
+        break;
+      i++;
+		}
+		if (combs[i][size] == inputs[2][i] && combs[i][size+1] == inputs[3][i])
+		{
+      j = -1;
+      while(++j< size)
+        res[riga][j] = combs[i][j];
+			excludecombs(combs, i);
+			if(recursive(res, combs, riga + 1, inputs))
+				return 1;
+			else
+				clearcombs(combs, i);	
+		}
+		i++;
+	}
+	return 0;
+
+}
+/************************************************************************************/
+int get_inputs(char *str, int **inputs, int size)
+{
+  int i;
+  int j;
+
+  i = 0;
+  while(i < 4)
+  {
+    j = 0;
+    while(j < size)
+    {
+      inputs[i][j] = str[2 * ((i * size) + j)] - '0';
+      if(i % 2 == 1)
+      {
+        if((inputs[i][j] + inputs[i - 1][j]) > size + 1
+        ||(inputs[i][j] + inputs[i - 1][j]) < 3)
+          return (0);
+      }
+      if(inputs[i][j] > size || inputs[i][j] == 0)
+        return (0);
+      j++;
+    }
+    i++;
+  }
+  return (1);
+}
+
+int checkArgs(char *str)
+{
+  int i;
+  int isValid;
+  int j;
+
+  j = 0;
+  i = 0;
+  isValid = 0;
+  while(*(str + i) != '\0')
+  {
+    if(*(str + i) < '0' || *(str + i) > '9')
+    {
+      if(*(str + i) == ' ')
+        if (isValid == 0)
+          isValid = 1;
+        else
+          return (0);
+      else
+        return (0);
+    }
+    else
+    {
+      j++;
+      isValid = 0;
+    }
+    i++;
+  }
+  i = 1;
+  while(i * i <= j)
+  {
+    if((i * i) == j)
+      return (i);
+    i++;
+  }
+  return 0;
+}
+
+void error(int n)
+{
+  char c = n + '0';
+  write(1, &c, 1);
+  write(1, "Error\n", 6);
+}
+
+int main(int argc, char **argv)
+{
+  if(argc != 2)
+  {
+    error(10 + argc);
+    return 0;
+  }
+  int max;
+  max = checkArgs(argv[1]);
+  if(max == 0)
+  {
+    error(2);
+    return 0;
+  }
+  /**/
     int **matrix  = (int **)(malloc(ft_factorial(max) * sizeof(int *)));
+    int **inputs  = (int **)(malloc(4 * sizeof(int *)));
+    int **res  = (int **)(malloc(max * sizeof(int *)));
     int i = 0;
+    int j = 0;
     while(i<ft_factorial(max))
     {
-        *(matrix + i) = (int *)(malloc((max + 2) * sizeof(int)));
+        *(matrix + i) = (int *)(malloc((max + 3) * sizeof(int)));//Dx -Sx - Used
         i++;
+    }
+    i = 0;
+    while(i < 4)
+    {
+      *(inputs + i) = (int *)(malloc(max * sizeof(int)));
+      i++;
+    }
+    i = 0;
+    while(i < max)
+    {
+      *(res + i) = (int *)(malloc(max * sizeof(int)));
+      i++;
+    }
+    if(get_inputs(argv[1], inputs, max) == 0)
+    {
+      error(3);
+      return (0);
     }
     gen_rec(matrix, 0, 0, max);
     generate_borders(matrix, max, ft_factorial(max));
     
-    int j = 0;
+    recursive(res, matrix, 0, inputs);
     printf("\n***********************\n");
     i = 0;
     while(i<ft_factorial(max))
     {
         j = 0;
-        while(j<max + 2)
+        while(j<max + 3)
         {
           if(j == max)
           {
@@ -146,244 +327,33 @@ int main()
         printf("\n");
         i++;
     }
-
-    return 0;
-}
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-int ft_factorial(int n)
-{
-    if(n == 0)
-        return 1;
-    return n * ft_factorial(n - 1);
-}
-
-int gen_n(int **matrix, int row, int col, int range)
-{
-    int n;
-    int j;
-    int i;
-    int fact;
-    int temp;
-
-    fact = ft_factorial(range - col - 1);
-    if(row == 0) //Se sto nello prima riga il numero sara' il primo valore della colonna
-        return (col + 1);
-    if(col == 0 && row >= fact)
-      return matrix[row - fact][col]+1;
-    n = 1;
-    j = 0;
-    while(j < col)//Cerco in tutta la riga il numero generato
-    {
-        if(matrix[row][j] == n)//Se lo trovo
-            {
-                n++;//Genero il prosimo
-                j = -1;//E torno all'inizio della riga
-            }
-        else
-        {
-            i = 0;
-            temp = row - fact;
-            while (temp >= 0 && i < (range - col - 1) && j != -1)
-            {
-                printf("temp => %d\n", temp);
-                if (matrix[temp][col] == n)
-                {
-                    n++;//Genero il prosimo
-                    j = -1;//E torno all'inizio della riga
-                }
-                temp -= fact;
-                i++;
-            }
-        }
-        j++;
-    }
-    printf("%d - %d - %d\n", row, col, n);
-    return n;//Il numero generato torna alla funzione chiamante
-}
-
-int gen_rec(int **matrix, int row, int col, int range)
-{
-    int i;
-    int fact;
-
-    if (col == (range - 1))
-    {
-        matrix[row][col] = gen_n(matrix, row, col, range);
-        return (row + 1);
-    }
-    else
-    {
-        i = 0;
-        fact = ft_factorial(range);
-        while (i < fact)
-        {
-            matrix[row][col] = gen_n(matrix, row, col, range);
-            if(row != gen_rec(matrix, row, col + 1, range))
-                if(col != 0)
-                    return row + 1;
-                row++;
-                    
-            i++;
-        }
-        return row;
-    }
-}
-
-int main()
-{
-  int max = 4;
-    int **matrix  = (int **)(malloc(ft_factorial(max) * sizeof(int *)));
-    int i = 0;
-    while(i<ft_factorial(max))
-    {
-        *(matrix + i) = (int *)(malloc(max * sizeof(int)));
-        i++;
-    }
-    gen_rec(matrix, 0, 0, max);
-    
-    int j = 0;
-    printf("\n***********************\n");
+        printf("\n");
+        printf("\n");
     i = 0;
-    while(i<ft_factorial(max))
+    while(i<4)
     {
         j = 0;
         while(j<max)
         {
-            printf("%d ", matrix[i][j]);
+            printf("%d ", inputs[i][j]);
             j++;
         }
         printf("\n");
         i++;
     }
-
-    return 0;
-}
-*/
-
-/******************************************************************************
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-*******************************************************************************/
-#include <stdio.h>
-#include <stdlib.h>
-
-int ft_factorial(int n)
-{
-    if(n == 0)
-        return 1;
-    return n * ft_factorial(n - 1);
-}
-
-void wrong_n(int *n, int *j, int *temp, int fact)
-{
-  *n += 1;//Genero il prosimo
-  *j = -1;//E torno all'inizio della riga
-  *temp = *temp - fact;
-}
-
-int gen_n(int **matrix, int row, int col, int range)
-{
-    int n;
-    int j;
-    int i;
-    int fact;
-    int temp;
-
-    fact = ft_factorial(range - col - 1);
-    n = 1;
-    j = -1;
-    /*if(row == 0) //Se sto nello prima riga il numero sara' il primo valore della colonna
-      return (col + 1);
-    if(col == 0 && row >= fact)
-      return matrix[row - fact][col]+1;*/
-    while(++j < col)//Cerco in tutta la riga il numero generato
-        if(matrix[row][j] == n)//Se lo trovo
-          wrong_n(&n, &j, &temp, fact);
-        else
-        {
-            i = -1;
-            temp = row - fact;
-            while (temp >= 0 && ++i < (range - col - 1) && j != -1)
-            {
-                if (matrix[temp][col] == n)
-                  wrong_n(&n, &j, &temp, fact);
-                temp -= fact;
-            }
-        }
-    return n;//Il numero generato torna alla funzione chiamante
-}
-
-int call_gen(int **matrix, int row, int col, int range)
-{
-    int fact;
-
-    fact = ft_factorial(range - col - 1);
-    if(row == 0) //Se sto nello prima riga il numero sara' il primo valore della colonna
-      return (col + 1);
-    if(col == 0 && row >= fact)
-      return matrix[row - fact][col]+1;
-    return (gen_n(matrix, row, col, range));
-}
-
-int gen_rec(int **matrix, int row, int col, int range)
-{
-    int i;
-    int fact;
-
-    if (col == (range - 1))
-    {
-        matrix[row][col] = call_gen(matrix, row, col, range);
-        return (row + 1);
-    }
-    else
-    {
-        i = 0;
-        fact = ft_factorial(range);
-        while (i < fact)
-        {
-            matrix[row][col] = call_gen(matrix, row, col, range);
-            if(row != gen_rec(matrix, row, col + 1, range))
-                if(col != 0)
-                    return row + 1;
-                row++;
-                    
-            i++;
-        }
-        return row;
-    }
-}
-
-int main()
-{
-  int max = 4;
-    int **matrix  = (int **)(malloc(ft_factorial(max) * sizeof(int *)));
-    int i = 0;
-    while(i<ft_factorial(max))
-    {
-        *(matrix + i) = (int *)(malloc(max * sizeof(int)));
-        i++;
-    }
-    gen_rec(matrix, 0, 0, max);
-    
-    int j = 0;
-    printf("\n***********************\n");
+        printf("\n");
+        printf("\n");
     i = 0;
-    while(i<ft_factorial(max))
+    while(i<max)
     {
         j = 0;
         while(j<max)
         {
-            printf("%d ", matrix[i][j]);
+            printf("%d ", res[i][j]);
             j++;
         }
         printf("\n");
         i++;
     }
-
     return 0;
 }
