33ebae3a87e98c2c17de0817d2c27e359bbf63dd
Nek97 <tortora.anielllo.ta@gmail.com>
2020-11-18 16:36:48 +0100

    C08 100%

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atortora <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/05 19:30:04 by atortora          #+#    #+#             */
+/*   Updated: 2020/11/06 10:02:12 by atortora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+# include <unistd.h>
+
+typedef	int	t_bool;
+
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN(x) (x % 2) == 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+#endif
