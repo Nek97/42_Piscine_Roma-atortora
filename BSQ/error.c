7ae9106a8868865d9cb78c1911de19b637b01e46
Nek97 <tortora.anielllo.ta@gmail.com>
2020-11-18 17:03:31 +0100

    100% by https://github.com/ekmbcd/Piscine-42Rome-2020, star him

---
+++ BSQ/error.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acilione <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 09:14:08 by acilione          #+#    #+#             */
+/*   Updated: 2020/11/12 09:14:48 by acilione         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+int		ft_print_error(char *err)
+{
+	write(2, err, ft_strlen(err));
+	return (0);
+}
+
+int		ft_close_file_err(char *err)
+{
+	close(g_fd);
+	write(2, err, ft_strlen(err));
+	return (0);
+}
