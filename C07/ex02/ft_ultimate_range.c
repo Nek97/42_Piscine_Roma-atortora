9ea0232b21a7d15f5f99e11876a575cdd90c4422
Nek97 <tortora.anielllo.ta@gmail.com>
2020-11-18 16:36:41 +0100

    C07 100%

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atortora <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 16:17:28 by atortora          #+#    #+#             */
+/*   Updated: 2020/11/04 21:18:08 by atortora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	if (!(*range = (int *)(malloc((max - min) * sizeof(int)))))
+		return (-1);
+	i = 0;
+	while (min < max)
+	{
+		(*range)[i] = min;
+		min++;
+		i++;
+	}
+	return (i);
+}
